4ca6bf93fa58c264f47484c7d215f9cdc63b22cf
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-12-05 10:32:36 +0000

    Day 5 part 2 - brute force correct answer in single digit minutes after been more careful about inclusive / exclusive ranges.

--- src/test/kotlin/Day5.kt
+++ src/test/kotlin/Day5.kt
@@ -13,46 +13,36 @@ class Day5 : Helpers {
 
     @Test
     fun part2() {
-        val example = parseAlmanac("day5.txt")
+        val almanac = parseAlmanac("day5example.txt")
 
+        // Build seed ranges
+        // There are no obvious overlaps; no wins from merging
         val seedRanges = mutableListOf<LongRange>()
         var i = 0
-        while (i < example.seeds.size) {
-            val first = example.seeds[i]
-            val length = example.seeds[i + 1]
+        while (i < almanac.seeds.size) {
+            val first = almanac.seeds[i]
+            val length = almanac.seeds[i + 1]
             seedRanges.add(first..first + length)
-            i +=2
-        }
-
-        println(seedRanges)
-        seedRanges.sortBy { it.first }
-            seedRanges.forEach {
-            println("" + it.first + " -> " + it.last  + ": " + (it.last - it.first))
+            i += 2
         }
 
         val total = seedRanges.map {
             it.last - it.first
         }.sum()
-        println(total)
-
         var min = Long.MAX_VALUE
         var c = 0L
         seedRanges.forEach { r ->
             val iterator = r.iterator()
-            while(iterator.hasNext()) {
+            while (iterator.hasNext()) {
                 val element = iterator.next()
-                val location = locationsFrom(listOf(element), example.mappings).first()
-                if (location <  min) {
-                    println("" + element + ": " + location)
+                val location = locationsFrom(listOf(element), almanac.mappings).first()
+                if (location < min) {
                     min = location
                 }
                 c++
-                if (c % 1000000L == 0L) {
-                    println((c * 1.0 / total) * 100)
-                }
             }
         }
-        println("MIN: " + min)
+        assertEquals(min, 46)
     }
 
     private fun locationsFrom(seeds: List<Long>, mappings: List<Mapping>): List<Long> {
@@ -62,11 +52,10 @@ class Day5 : Helpers {
             mappings.forEach { mapping ->
                 // Is there a mapping which catches this input
                 val matchingRange = mapping.ranges.find { r ->
-                    val inputRange = r.source..r.source + r.size
-                    s in inputRange
+                    s in r.source
                 }
                 if (matchingRange != null) {
-                    val delta = matchingRange.destination - matchingRange.source
+                    val delta = matchingRange.delta
                     val a = s + delta
                     if (delta > 0 && a < s) {
                         throw RuntimeException()
@@ -91,16 +80,20 @@ class Day5 : Helpers {
             var rangeLine = lines.next()
             while (rangeLine.isNotBlank() && lines.hasNext()) {
                 val fields = rangeLine.split(" ").map { it.toLong() }
-                ranges.add(Range(fields[0], fields[1], fields[2]))
+                val size = fields[2]
+                val destStart = fields[0]
+                val sourceStart = fields[1]
+                val delta = destStart - sourceStart
+                // inclusive vs exclusive is important in part2
+                ranges.add(Range(delta, sourceStart..<(sourceStart + size)))
                 rangeLine = lines.next()
             }
             mappings.add(Mapping(name, ranges.toList()))
         }
-
         return Almanac(seeds, mappings)
     }
 }
 
 data class Almanac(val seeds: List<Long>, val mappings: List<Mapping>)
 data class Mapping(val name: String, val ranges: List<Range>)
-data class Range(val destination: Long, val source: Long, val size: Long)
+data class Range(val delta: Long, val source: LongRange)
