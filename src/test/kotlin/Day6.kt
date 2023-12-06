88a7e29b7faa5458858a8624b28e5f7c40ee1147
Tony McCrae <tony.mccrae@theguardian.com>
2023-12-06 11:19:29 +0000

    Day 6 part 2.
    Upgrading to Long to accomodate the Int wrap around.

--- src/test/kotlin/Day6.kt
+++ src/test/kotlin/Day6.kt
@@ -9,20 +9,43 @@ class Day6 : Helpers {
         assertEquals(marginOfWinning(parseRaces("day6.txt")), 2344708)
     }
 
-    private fun marginOfWinning(races: List<Pair<Int, Int>>) = races.map { race ->
+    @Test
+    fun part2() {
+        assertEquals(kernedWins(parseRaces("day6example.txt")), 71503)
+        assertEquals(kernedWins(parseRaces("day6.txt")), 30125202)
+    }
+
+    private fun marginOfWinning(races: List<Pair<Long, Long>>) = races.map { race ->
+        numberOfWaysToWin(race)
+    }.reduce { acc, i -> acc * i }
+
+    private fun kernedWins(races: List<Pair<Long, Long>>): Int {
+        val time = StringBuffer()
+        val distance = StringBuffer()
+        races.forEach { race ->
+            time.append(race.first)
+            distance.append(race.second)
+        }
+        val kerned = Pair(time.toString().toLong(), distance.toString().toLong())
+        return numberOfWaysToWin(kerned)
+    }
+
+    private fun numberOfWaysToWin(race: Pair<Long, Long>): Int {
         val duration = race.first
         val recordDistance = race.second
-        (0..duration).map {
+        return (0..duration).map {
             val v = it
             val runningTime = duration - it
             v * runningTime
         }.filter { it > recordDistance }.size
-    }.reduce { acc, i -> acc * i }
+    }
 
-    private fun parseRaces(filename: String): List<Pair<Int, Int>> {
+    private fun parseRaces(filename: String): List<Pair<Long, Long>> {
         val lines = stringsFromFile(filename)
-        val times = lines.first().split(":")[1].replace(multipleBlankSpace, " ").replaceFirst(" ", "").split(" ").map { it.toInt() }
-        val distances = lines.last().split(":")[1].replace(multipleBlankSpace, " ").replaceFirst(" ", "").split(" ").map { it.toInt() }
+        val times = lines.first().split(":")[1].replace(multipleBlankSpace, " ").replaceFirst(" ", "").split(" ")
+            .map { it.toLong() }
+        val distances = lines.last().split(":")[1].replace(multipleBlankSpace, " ").replaceFirst(" ", "").split(" ")
+            .map { it.toLong() }
         return times.zip(distances)
     }
 }
