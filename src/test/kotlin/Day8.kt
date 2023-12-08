e29416aa3d87ce8c7dba5081fbf88416def238bb
Tony McCrae <tony@eelpieconsulting.co.uk>
2023-12-08 09:46:40 +0000

    Day 8 part 2. Force the example only.

--- src/test/kotlin/Day8.kt
+++ src/test/kotlin/Day8.kt
@@ -4,10 +4,45 @@ import org.testng.annotations.Test
 class Day8 : Helpers {
 
     @Test
-    fun part() {
+    fun part1() {
         assertEquals(travelFromTo(parseMap("day8example.txt")), 2)
         assertEquals(travelFromTo(parseMap("day8.txt")), 19783)
     }
+
+    @Test
+    fun part2() {
+        val game = parseMap("day8example2.txt")
+        // Find starting nodes.
+        val startingNodes = game.nodes.values.filter { it.id.endsWith("A") }.toList()
+
+        var current = startingNodes.toList()
+        var i = 0L
+        var isFinished = current.all{ it.id.endsWith("Z")}
+        while (!isFinished) {
+            // Tell all nodes to take a step
+            val nextTurnIndex = i % game.turns.length
+            val nextTurn = game.turns[nextTurnIndex.toInt()]
+            current = current.map { node ->
+                step(nextTurn, node, game)
+            }
+            i += 1
+            isFinished = current.all{ it.id.endsWith("Z")}
+        }
+        assertEquals(i, 6)
+    }
+
+    private fun step(nextTurn: Char, current: Node, game: Game): Node {
+        var current1 = current
+        val nextNode = if (nextTurn == 'L') {
+            current1.turns.first()
+        } else {
+            current1.turns.last()
+        }
+        current1 = game.nodes[nextNode]!!
+        return current1
+    }
+
+
 
     private fun travelFromTo(game: Game): Int {
         val start = "AAA"
@@ -19,16 +54,13 @@ class Day8 : Helpers {
         while (current.id != end) {
             val nextTurnIndex = i % game.turns.length
             val nextTurn = game.turns[nextTurnIndex]
-            val nextNode = if (nextTurn == 'L') {
-                current.turns.first()
-            } else {
-                current.turns.last()
-            }
-            current = game.nodes[nextNode]!!
+            current = step(nextTurn, current, game)
             i += 1
         }
         return i
     }
+
+
 
     fun parseMap(filename: String): Game {
         val lines = stringsFromFile(filename).iterator()
